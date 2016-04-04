a842d5644ab88cd9d43e8d815ce566ef1b466c76
Valentin Sasyan <valentin.sasyan@gmail.com>
2016-04-04 17:39:01 +0200

    maj .gitignore et generation première entite (postgis ok)

---
+++ Symfony/src/AppBundle/Entity/particulier.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * particulier
+ *
+ * @ORM\Table(name="particulier")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\particulierRepository")
+ */
+class particulier
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastname", type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="signin_date", type="datetime")
+     */
+    private $signinDate;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="activated", type="boolean")
+     */
+    private $activated;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return particulier
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return particulier
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return particulier
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return particulier
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set signinDate
+     *
+     * @param \DateTime $signinDate
+     *
+     * @return particulier
+     */
+    public function setSigninDate($signinDate)
+    {
+        $this->signinDate = $signinDate;
+
+        return $this;
+    }
+
+    /**
+     * Get signinDate
+     *
+     * @return \DateTime
+     */
+    public function getSigninDate()
+    {
+        return $this->signinDate;
+    }
+
+    /**
+     * Set activated
+     *
+     * @param boolean $activated
+     *
+     * @return particulier
+     */
+    public function setActivated($activated)
+    {
+        $this->activated = $activated;
+
+        return $this;
+    }
+
+    /**
+     * Get activated
+     *
+     * @return bool
+     */
+    public function getActivated()
+    {
+        return $this->activated;
+    }
+}
