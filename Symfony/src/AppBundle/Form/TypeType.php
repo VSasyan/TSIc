bad095c2c91e43c692227dab9245bacfee423cda
Valentin Sasyan <valentin.sasyan@gmail.com>
2016-04-05 15:34:29 +0200

    Ajout Type

---
+++ Symfony/src/AppBundle/Form/TypeType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TypeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', array('label' => 'Nom'))
+            ->add('description', 'textarea', array('label' => 'Description'))
+            ->add('typeLogoPath', 'file', array('label' => 'Logo', 'required' => false))
+            ->add('save', 'submit', array('label' => 'Sauver'))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Type'
+        ));
+    }
+}
