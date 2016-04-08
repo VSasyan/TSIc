ccce05ab9f002cf9aa59bbe2285654d616c4b4be
Alban Kraus <alban.kraus@ensg.eu>
2016-04-08 15:15:28 +0200

    Simplified add_perturbation

--- Symfony/web/js/init_add_perturbation.js
+++ Symfony/web/js/init_add_perturbation.js
@@ -35,37 +35,3 @@ function setMarqueur() {
 	$('#formulation_center').val("SRID=4326;POINT(" + position.lng + " " + position.lat + ")");
 	$('#formulation_geoJSON').val("POINT(" + position.lng + " " + position.lat + ")");
 }
-
-function coordinatesToWKT(coords) {
-	if('latitude' in coords && 'longitude' in coords) {
-		return "POINT(" + coords.longitude + " " + coords.latitude + ")";
-	} else {
-		console.error("Bad conversion from coordinate object.");
-		return false;
-	}
-}
-
-function geolocation(callback) {
-	if ("geolocation" in navigator) {
-		/* geolocation is available */
-		var options = {
-			enableHighAccuracy : true,
-			timeout : 8000, // 8 sec
-			maximumAge : 8000 // 8 sec
-		};
-		positionWatchID = navigator.geolocation.watchPosition(callback, locationError, options);
-		return true;
-	} else {
-		/* geolocation IS NOT available */
-		return false;
-	}
-}
-
-function locationError(error) {
-	console.error("Failed geolocation: " + error.code + " : " + error.message);
-
-	/* Development on a machine without capabilities: simulate success */
-	//$('#ajax_loader').load("erreur");
-	getLocation({ coords: { latitude: 48.8410544, longitude: 2.5873005 },});
-	/* End of development portion */
-}
