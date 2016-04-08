9ae1424642726fc2fc85f76446614f179ba86851
Alban Kraus <alban.kraus@ensg.eu>
2016-04-08 15:16:04 +0200

    Tabified files

--- Symfony/web/js/init_ajax.js
+++ Symfony/web/js/init_ajax.js
@@ -1,26 +1,26 @@
 // Functions
 function getPage(route, after) {
-    return $.get(route, function(data, status) {
-	document.title = data.title;
-	$('#ajax_loader').html(data.content);
-	if(after != false) { after(); }
-    });
+	return $.get(route, function(data, status) {
+		document.title = data.title;
+		$('#ajax_loader').html(data.content);
+		if(after != false) { after(); }
+	});
 }
 
 
 function listNearest(pos) {
-    getPage(
-	Routing.generate("perturbation_list_nearest", { position : coordinatesToWKT(pos.coords), radius : 1000 }),
-	function() { initMap(); }
-    );
-    navigator.geolocation.clearWatch(positionWatchID);
-    positionWatchID = false;
+	getPage(
+		Routing.generate("perturbation_list_nearest", { position : coordinatesToWKT(pos.coords), radius : 1000 }),
+		function() { initMap(); }
+	);
+	navigator.geolocation.clearWatch(positionWatchID);
+	positionWatchID = false;
 }
 
 var functions = {
-    listNearest : function() { geolocation(listNearest); },
+	listNearest : function() { geolocation(listNearest); },
 };
 
 document.addEventListener("DOMContentLoaded", function() {
-    functions[$('#ajax_loader').data('function')]();
+	functions[$('#ajax_loader').data('function')]();
 });
