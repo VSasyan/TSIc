1f2d921160b0c8f462bf2fdf3049df86e7844c3a
Alban Kraus <alban.kraus@ensg.eu>
2016-04-12 17:04:20 +0200

    Created GeneralisedLink (testing version)
    Element is the @Entity -> Fail.

--- Symfony/src/NetworkBundle/Controller/DefaultController.php
+++ Symfony/src/NetworkBundle/Controller/DefaultController.php
@@ -5,13 +5,22 @@ namespace NetworkBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use NetworkBundle\Entity\GeneralisedLink;
+
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
-    {
+	/**
+	 * @Route("/")
+	 */
+	public function indexAction()
+	{
+		$truc = new GeneralisedLink();
+		$truc->setToto(1);
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($truc);
+		$em->flush();
+
         return $this->render('NetworkBundle:Default:index.html.twig');
     }
 }
---
+++ Symfony/src/NetworkBundle/Entity/GeneralisedLink.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace NetworkBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use NetworkBundle\Entity\Element;
+
+/**
+ * GeneralisedLink
+ */
+class GeneralisedLink extends Element
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="toto", type="integer")
+     */
+    private $toto;
+
+
+    /**
+     * Get toto
+     *
+     * @return int
+     */
+    public function getToto()
+    {
+        return $this->toto;
+    }
+
+    /**
+     * Set toto
+     *
+     * @param int $toto
+     *
+     * @return GeneralisedLink
+     */
+    public function setToto($toto)
+    {
+        $this->toto = $toto;
+
+        return $this;
+    }
+}
+
