5c35fc77845c15e5c028ed19e5f32951b742e0e8
Alban Kraus <alban.kraus@ensg.eu>
2016-04-13 14:17:59 +0200

    Development version of RoadLink.

--- Symfony/src/TransportBundle/Controller/DefaultController.php
+++ Symfony/src/TransportBundle/Controller/DefaultController.php
@@ -5,13 +5,22 @@ namespace TransportBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use TransportBundle\Entity\RoadLink;
+
 class DefaultController extends Controller
 {
-	/**
-	 * @Route("/")
-	 */
-	public function indexAction()
-	{
-		return $this->render('TransportBundle:Default:index.html.twig');
-	}
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+		$truc = new RoadLink;
+		$truc->setGeographicalName("Bonjour");
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($truc);
+		$em->flush();
+        
+        return $this->render('TransportBundle:Default:index.html.twig', array("name" => $truc->getGeographicalName()));
+    }
 }
