e4ac6d85773eb31555de27aad8ee5087e4b3c222
Aiache <aiache@del0801w149.ensg.eu>
2016-04-13 16:42:37 +0200

    Add TransportNode and RoadNode entities

---
+++ Symfony/src/TransportBundle/Entity/RoadNode.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace NetworkBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * RoadNode
+ *
+ * @ORM\Table(name="road_node")
+ * @ORM\Entity(repositoryClass="NetworkBundle\Repository\RoadNodeRepository")
+ */
+class RoadNode extends TransportNode
+{
+    
+}
+
