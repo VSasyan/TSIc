0402d58890500ada6149b1d808e09380525753d1
Aiache <aiache@del0801w149.ensg.eu>
2016-04-13 16:42:37 +0200

    Add TransportNode and RoadNode entities

---
+++ Symfony/src/TransportBundle/Entity/TransportNode.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TransportBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TrasportNode
+ *
+ * @ORM\Table(name="trasport_node")
+ * @ORM\Entity(repositoryClass="TransportBundle\Repository\TrasportNodeRepository")
+ */
+absract class TransportNode extends TransportObject
+{
+    
+}
+
