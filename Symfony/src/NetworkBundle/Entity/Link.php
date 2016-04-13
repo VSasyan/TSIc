dd8583c616d6059ee20c5c9c1f633ec22fbed838
Alban Kraus <alban.kraus@ensg.eu>
2016-04-13 15:43:04 +0200

    Prepared relationship with Node
    + re-tabified

--- Symfony/src/NetworkBundle/Entity/Link.php
+++ Symfony/src/NetworkBundle/Entity/Link.php
@@ -14,36 +14,48 @@ use NetworkBundle\Entity\GeneralisedLink;
  */
 class Link extends GeneralisedLink
 {
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="centrelineGeometry", type="string", length=4096, nullable=true)
-     */
-    private $centrelineGeometry;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="centrelineGeometry", type="string", length=4096, nullable=true)
+	 */
+	private $centrelineGeometry;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="Node", inversedBy="spokeStart")
+	 * @ORM\JoinColumn(name="startNode", referencedColumnName="inspireId")
+	 */
+	protected $startNode;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="Node", inversedBy="spokeEnd")
+	 * @ORM\JoinColumn(name="endNode", referencedColumnName="inspireId")
+	 */
+	protected $endNode;
 
 
-    /**
-     * Set centrelineGeometry
-     *
-     * @param string $centrelineGeometry
-     *
-     * @return Link
-     */
-    public function setCentrelineGeometry($centrelineGeometry)
-    {
-        $this->centrelineGeometry = $centrelineGeometry;
+	/**
+	 * Set centrelineGeometry
+	 *
+	 * @param string $centrelineGeometry
+	 *
+	 * @return Link
+	 */
+	public function setCentrelineGeometry($centrelineGeometry)
+	{
+		$this->centrelineGeometry = $centrelineGeometry;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Get centrelineGeometry
-     *
-     * @return string
-     */
-    public function getCentrelineGeometry()
-    {
-        return $this->centrelineGeometry;
-    }
+	/**
+	 * Get centrelineGeometry
+	 *
+	 * @return string
+	 */
+	public function getCentrelineGeometry()
+	{
+		return $this->centrelineGeometry;
+	}
 }
 
