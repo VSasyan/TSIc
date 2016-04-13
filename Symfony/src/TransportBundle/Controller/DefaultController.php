2ccac0ef751a00390ed8bcabd6032a2aa2a4546d
Alban Kraus <alban.kraus@ensg.eu>
2016-04-13 14:07:28 +0200

    Removed developement version of TransportLink

--- Symfony/src/TransportBundle/Controller/DefaultController.php
+++ Symfony/src/TransportBundle/Controller/DefaultController.php
@@ -5,22 +5,13 @@ namespace TransportBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-use TransportBundle\Entity\TransportLink;
-
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
-    {
-		$truc = new TransportLink;
-		$truc->setGeographicalName("Bonjour");
-
-		$em = $this->getDoctrine()->getManager();
-		$em->persist($truc);
-		$em->flush();
-        
-        return $this->render('TransportBundle:Default:index.html.twig', array("name" => $truc->getGeographicalName()));
-    }
+	/**
+	 * @Route("/")
+	 */
+	public function indexAction()
+	{
+		return $this->render('TransportBundle:Default:index.html.twig');
+	}
 }
