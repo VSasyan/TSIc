d9bf67db3eb0187c30d0aef5dbd62350dc91447a
Valentin Sasyan <valentin.sasyan@gmail.com>
2016-04-20 17:06:22 +0200

    Correctio conflits suite (à confirmer ?)

--- Symfony/src/TransportBundle/Entity/WeatherCondition.php
+++ Symfony/src/TransportBundle/Entity/WeatherCondition.php
@@ -14,40 +14,6 @@ use TransportBundle\Entity\TransportProperty;
  */
 class WeatherCondition extends TransportProperty
 {
-<<<<<<< HEAD
-	/**
-	 * @var string
-	 *
-	 * @ORM\Column(name="name", type="string", length=255)
-	 */
-	private $name;
-
-
-
-	/**
-	 * Set name
-	 *
-	 * @param string $name
-	 *
-	 * @return WeatherCondition
-	 */
-	public function setName($name)
-	{
-		$this->name = $name;
-
-		return $this;
-	}
-
-	/**
-	 * Get name
-	 *
-	 * @return string
-	 */
-	public function getName()
-	{
-		return $this->name;
-	}
-=======
     /**
     * @ORM\ManyToOne(targetEntity="WeatherConditionValue", cascade={"persist"})
     * @ORM\JoinColumn(nullable=false)
@@ -79,6 +45,5 @@ class WeatherCondition extends TransportProperty
     {
         return $this->weatherConditionValue;
     }
->>>>>>> 1bf6482940389076a985e334006bd7f46cee12e3
 }
 
