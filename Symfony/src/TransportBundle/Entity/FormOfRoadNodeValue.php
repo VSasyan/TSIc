a6491028c9ace43ee6bc3e12a17dd8093bd9ece6
Aiache <aiache@del0801w149.ensg.eu>
2016-04-20 10:00:01 +0200

    Added the Entity FormOfRoadNodeValue
    Enum for formOfRoadNode attribute

---
+++ Symfony/src/TransportBundle/Entity/FormOfRoadNodeValue.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace TransportBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * FormOfRoadNodeValue
+ *
+ * @ORM\Table(name="form_of_road_node_value")
+ * @ORM\Entity(repositoryClass="TransportBundle\Repository\FormOfRoadNodeValueRepository")
+ */
+class FormOfRoadNodeValue
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return FormOfRoadNodeValue
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
